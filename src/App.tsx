8b507a49d7aa8ba1fe672e4158982cc2e0617627
jules <jules.bloux@etu.uca.fr>
2022-04-12 17:02:07 +0200

    Premier travail: site responsive et répartition de l'espace

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import SingleSetting from  './components/singleSetting'
+import SingleSetting from './components/singleSetting';
+import Settings from './components/settings';
 
 function App() {
-
   return (
-    <div className='bg-black'>
-      <h1 className='font-sans'>toto</h1>
-      <SingleSetting/>
+    <div className="flex bg-black border border-red-500 h-screen flex-row ">
+      <div className="flex bg-gray-800-800 border border-red-500 gap-4 flex-col flex-grow ">
+        <div className="flex bg-green-500 border border-red-500 gap-4 flex-grow-0">
+          <video className='w-4/5 ' src="https://file-examples.com/storage/fe3f5fc9646254aafa134d8/2017/04/file_example_MP4_1920_18MG.mp4" autoPlay muted loop />
+          </div>
+        <div className="flex bg-purple-500 border border-red-500 gap-4 flex-row h-1/5 content-start">
+          <div className="flex bg-blue-500">RUSH 1</div>
+          <div className="flex bg-blue-500">RUSH 2</div>
+          <div className="flex bg-blue-500">RUSH 3</div>
+        </div>
+      </div>
+      <div className="flex bg-orange-500 border border-red-500 gap-4 flex-col w-40  items-center  content-start flex-shrink-0">
+        <h1>SETTINGS</h1>
+        <div className="flex bg-blue-500">SETTING 1</div>
+        <div className="flex bg-blue-500">SETTING 2</div>
+        <div className="flex bg-blue-500">SETTING 3</div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/singleSetting.tsx
+++ src/components/singleSetting.tsx
@@ -1,22 +1,39 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 
 
 const SingleSetting = () => {
-    const [title, setTitle] = useState<string>("")
-    const [activate, setActivate] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>("Titre")
+    const [activate, setActivate] = useState<boolean>(true)
+    const [level, setLevel] = useState<number>()
+
+    const handleClick = useCallback(() =>{
+        activate ? setActivate(false) : setActivate(true);
+    }, [activate]);
+
 
 
     return(
-        <div className="rounded bg-violet-500"> 
-            <h3 className="top-0">{title}</h3>
-            {activate?(
-                <div className="bg-lime-400 rounded"></div>
-            ):<div className="bg-red-500 rounded"></div>
-            }
+        <div className="rounded-2xl bg-violet-500 grid grid-cols-8 grid-rows-5 max-w-[15%] " onClick={handleClick}> 
+            <div className="col-start-2 col-span-4 justify-self-start">
+                <h1 className="font-mono text-black text-4xl">{title}</h1>
+            </div>
+            <div className="col-start-2 ">
+                <h3 className="font-mono text-white  text-xl italic ">Value</h3>
+            </div>
+            <div className="col-start-2 col-span-2 row-start-3">
+                <input type={"checkbox"} className=" bg-slate-500 w-full h-full" />
+            </div>
+            <div className="col-start-5 col-span-4 row-start-3 row-span-2">
+                {activate?(
+                    <div className="bg-lime-400 rounded-full  w-full h-full"></div>
+                 ):<div className="bg-red-500 rounded-full  w-full h-full"></div>
+                }
+            </div>
             
-
-
+            
+            
+            
         </div>
     );
 }
